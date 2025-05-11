c89d17301e629e4787296c68f33b2988991b4cb4
Shiloh <shilohe.ai@gmail.com>
2025-05-11 15:14:30 +0100

    Comapatible settings/staging.py file for beta deployment

--- ecommerce_backend/ecommerce_backend/settings/staging.py
+++ ecommerce_backend/ecommerce_backend/settings/staging.py
@@ -4,7 +4,7 @@ from decouple import config
 
 # Django settings
 DEBUG = config('DEBUG', cast=bool, default=False)
-ALLOWED_HOSTS = ['http://54.196.149.31']
+ALLOWED_HOSTS = ['54.196.149.31', 'medinn.duckdns.org']
 
 # Database
 DATABASES = {
@@ -14,11 +14,25 @@ DATABASES = {
     }
 }
 
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    "https://medinn.duckdns.org"
+]
+
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "https://medinn-virid.vercel.app",
+    'https://medinn.duckdns.org',
 ]
 
 CORS_TRUSTED_ORIGINS = [
     "https://medinn-virid.vercel.app",
+    'https://medinn.duckdns.org',
 ]
+
+# SSL Settings
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
